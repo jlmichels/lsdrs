aaebfb18ac9898a34509151993b86f8f3aeafd35
John L. Michels <jlmichels@gmail.com>
2022-06-19 08:33:19 +0900

    Implement listSamples export/import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import ListSamples from './listSamples';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <h1>LSDRS</h1>
         Laboratory Sample Drop-off/Reception System
       </header>
+      <ListSamples/>
     </div>
   );
 }
---
+++ src/listSamples.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ListSamples() {
+    return (
+        <React.Fragment>
+            Listing Samples 123
+        </React.Fragment>
+    );
+};
+
+export default ListSamples;
