64d16c807fab9bf1645f1bf70b805c1e47dac59c
John L. Michels <jlmichels@gmail.com>
2022-06-18 15:26:54 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>LSDRS</h1>
+        Laboratory Sample Drop-off/Reception System
+      </header>
+    </div>
+  );
+}
+
+export default App;
