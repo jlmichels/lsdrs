be52ded4f48e4782b8d4d43781cc3fcce8861880
John L. Michels <jlmichels@gmail.com>
2022-06-21 13:01:12 +0900

    Make DB pool use entirely env variables

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: './.env' });
 const Pool = require('pg').Pool
 const pool = new Pool({
     user: process.env.DATABASE_USER,
-    host: 'localhost',
+    host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
