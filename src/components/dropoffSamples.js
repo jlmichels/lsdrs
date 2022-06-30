9c0bd6d1044cedc176b710afc5e16c5c39c742c3
John L. Michels <jlmichels@gmail.com>
2022-06-30 16:42:29 +0900

    Add material button generation logic

--- src/components/dropoffSamples.js
+++ src/components/dropoffSamples.js
@@ -1,9 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Button from 'react-bootstrap/Button';
 
 const DropoffSamples = () => {
+    const [dropoffMaterial, setDropoffMaterial] = useState();
+    const [dropoffQuantity, setDropoffQuantity] = useState();
+    const [dropoffLot, setDropoffLot] = useState();
+
+    /* TODO:
+        Modal screen that displayed material, and allows input of lot and selection of quantity (10mg, 50mg, 200g, 2kg)
+        Auth to get user
+        Banner after successful dropoff that fades from view
+    */
+
+    const materials = ["A1", "B2", "C3", "D4", "E5", "F6"];
+
     return(
         <Fragment>
-            Dropping off samples!
+            Select Material
+            <div className="d-flex justify-content-center flex-wrap">
+                {materials.map((material) => (
+                    <Button variant="outline-primary" size="lg" onClick={() => setDropoffMaterial(material)}>{material}</Button>    
+                ))};
+            </div>
         </Fragment>
     );
 }
