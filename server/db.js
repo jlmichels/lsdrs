174e5ac8d18bd092bfe3437d35b9d45b603c6c22
John L. Michels <jlmichels@gmail.com>
2022-07-03 20:18:57 +0900

    Attempt Heroku DB connection

--- server/db.js
+++ server/db.js
@@ -1,12 +1,17 @@
 require('dotenv').config({ path: './.env' });
 /* Using .env variables: process.env.TEST1 */
+console.log("process.env.DATABASE_URL");
+console.log(process.env.DATABASE_URL);
 const Pool = require('pg').Pool
 const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+    /*
     user: process.env.DATABASE_USER,
     host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
+    */
 });
 
 module.exports = pool;
